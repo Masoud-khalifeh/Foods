9c8f3c2f5cd22aa284a19a05176d07dc4a0b9df2
Masoud Khalifeh <masoud.khalifeh28@gmail.com>
2023-07-06 02:51:53 +0200

    Add Favorite screnn layout

--- Screens/Favorites.js
+++ Screens/Favorites.js
@@ -1,11 +1,38 @@
 import { View, Text } from "react-native";
+import { MEALS } from '../data/dummy-data';
+import FoodGrid from "../components/FoodGrid";
+import { FlatList } from "react-native";
+import { StyleSheet } from "react-native";
+import {colors} from '../data/Colors'
 
-export default function Favorites (){
+
+export default function Favorite({  navigation }) {
+    
+
+    function foodPressHandler(id,title) {
+        navigation.navigate("FoodDetails", { id: id,title: title })
+    }
+console.log(MEALS)
+    return (
+        <View style={styles.container}>
+
+            <FlatList contentContainerStyle={styles.flatlist} data={MEALS} keyExtractor={(item) => item.id} renderItem={({ item }) => (
+                <FoodGrid {...item} onPress={() => foodPressHandler(item.id,item.title)} />
+            )} />
 
 
-    return(
-        <View>
-            <Text>HI, Here is Favorite Page</Text>
         </View>
+
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: colors.primary,
+    },
+    flatlist: {
+        paddingBottom: 30
+    }
+})
